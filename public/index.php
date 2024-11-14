131d8fb5d1946b4baee4540d4b0a9dfa2b9dc208
iakhator <iakhator@gmail.com>
2024-11-14 13:44:27 +0100

    load env variables

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
+use Dotenv\Dotenv;
 
+use DI\Container;
 use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
 use InertiaAdapter\InertiaFactory;
@@ -8,13 +10,27 @@ use InertiaAdapter\Inertia;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$container = new Container();
+AppFactory::setContainer($container);
+
 $app = AppFactory::create();
 
-// Set up Twig for rendering
-$twig = Twig::create(__DIR__ . '/../src/views');
-$twigRenderer = new TwigRenderer($twig);
+// Set up Twig for rendering and add it to the container
+$twig = Twig::create(__DIR__ . '/../src/views', ['cache' => false]);
+$container->set(Twig::class, $twig); // Register Twig in the container first
 
-// Initialize Inertia
+// Add global environment variables to Twig
+$container->get(Twig::class)->getEnvironment()->addGlobal('app', [
+    'environment' => $_ENV['APP_ENV'] ?? 'production',
+    'debug' => $_ENV['APP_DEBUG'] ?? false,
+    'left' => 'left',
+]);
+
+// Initialize Inertia with the Twig renderer
+$twigRenderer = new TwigRenderer($twig);
 InertiaFactory::initialize($twigRenderer);
 
 // Share common props across all pages
@@ -25,8 +41,6 @@ Inertia::share([
         'role' => 'Admin'
     ],
 ]);
-
-// Include routes from a separate file
 
 // Load routes
 (require __DIR__ . '/../app/routes.php')($app);
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,13 +7,13 @@ namespace Composer\Autoload;
 class ComposerStaticInit51c3ef6cf5c7e85123544049b3790afd
 {
     public static $files = array (
+        '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
+        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         '23c18046f52bef3eea034657bafda50f' => __DIR__ . '/..' . '/symfony/polyfill-php81/bootstrap.php',
         '253c157292f75eb38082b5acb06f3f01' => __DIR__ . '/..' . '/nikic/fast-route/src/functions.php',
         '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
-        '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
-        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
-        '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
         'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
+        '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
         '89efb1254ef2d1c5d80096acd12c4098' => __DIR__ . '/..' . '/twig/twig/src/Resources/core.php',
         'ffecb95d45175fd40f75be8a23b34f90' => __DIR__ . '/..' . '/twig/twig/src/Resources/debug.php',
         'c7baa00073ee9c61edf148c51917cfb4' => __DIR__ . '/..' . '/twig/twig/src/Resources/escaper.php',
@@ -42,6 +42,7 @@ class ComposerStaticInit51c3ef6cf5c7e85123544049b3790afd
             'Psr\\Http\\Server\\' => 16,
             'Psr\\Http\\Message\\' => 17,
             'Psr\\Container\\' => 14,
+            'PhpOption\\' => 10,
         ),
         'L' => 
         array (
@@ -52,6 +53,10 @@ class ComposerStaticInit51c3ef6cf5c7e85123544049b3790afd
             'Invoker\\' => 8,
             'InertiaAdapter\\' => 15,
         ),
+        'G' => 
+        array (
+            'GrahamCampbell\\ResultType\\' => 26,
+        ),
         'F' => 
         array (
             'Fig\\Http\\Message\\' => 17,
@@ -59,6 +64,7 @@ class ComposerStaticInit51c3ef6cf5c7e85123544049b3790afd
         ),
         'D' => 
         array (
+            'Dotenv\\' => 7,
             'DI\\' => 3,
         ),
         'A' => 
@@ -111,12 +117,16 @@ class ComposerStaticInit51c3ef6cf5c7e85123544049b3790afd
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Container\\' => 
         array (
             0 => __DIR__ . '/..' . '/psr/container/src',
+        ),
+        'PhpOption\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpoption/phpoption/src/PhpOption',
         ),
         'Laravel\\SerializableClosure\\' => 
         array (
@@ -130,6 +140,10 @@ class ComposerStaticInit51c3ef6cf5c7e85123544049b3790afd
         array (
             0 => __DIR__ . '/../..' . '/inertia',
         ),
+        'GrahamCampbell\\ResultType\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/graham-campbell/result-type/src',
+        ),
         'Fig\\Http\\Message\\' => 
         array (
             0 => __DIR__ . '/..' . '/fig/http-message-util/src',
@@ -137,6 +151,10 @@ class ComposerStaticInit51c3ef6cf5c7e85123544049b3790afd
         'FastRoute\\' => 
         array (
             0 => __DIR__ . '/..' . '/nikic/fast-route/src',
+        ),
+        'Dotenv\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/vlucas/phpdotenv/src',
         ),
         'DI\\' => 
         array (
